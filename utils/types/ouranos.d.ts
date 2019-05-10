685128dbc282846dd036fdda7ce1862d72e3c53f
Pascal ECHEMANN <pascal@wooz.io>
2019-05-10 15:06:26 +0200

    gitignore update
    Fix util s folder import.

---
+++ utils/types/ouranos.d.ts
@@ -0,0 +1,12 @@
+/*!
+ * Asteria Ouranos Node Module
+ * Copyright(c) 2019 Pascal ECHEMANN
+ * Apache 2.0 Licensed
+ * This is a part of the Asteria Project: <https://github.com/asteria-project>
+ */
+
+declare module "asteria-ouranos" {
+
+import {AbstractAsteriaObject, AsteriaSessionConfig, AsteriaSession, StreamProcessConfig, StreamProcess, AsteriaContext, AsteriaLogger, StreamProcessor, AsteriaFilter, FilterOperator, FilterOperatorNotation, AsteriaStream, AsteriaErrorCode, AsteriaError, StreamProcessBuilder, AsteriaLogLevel} from "asteria-gaia";
+
+/*PLACEHOLDER*/}

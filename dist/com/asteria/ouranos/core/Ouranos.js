974bebadd15168f20bf67e546afa0bb2ce3e807f
Pascal ECHEMANN <pascal@wooz.io>
2019-05-12 10:57:52 +0200

    Errors management
    Add support for errors management.

--- dist/com/asteria/ouranos/core/Ouranos.js
+++ dist/com/asteria/ouranos/core/Ouranos.js
@@ -3,15 +3,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const asteria_gaia_1 = require("asteria-gaia");
 const OuranosSession_1 = require("./OuranosSession");
 const OuranosProcessBuilder_1 = require("../util/builder/OuranosProcessBuilder");
+const OuranosLogger_1 = require("../util/logging/OuranosLogger");
+const OuranosErrorBuilder_1 = require("../util/builder/OuranosErrorBuilder");
 class Ouranos extends asteria_gaia_1.AbstractAsteriaObject {
     constructor() {
-        super('com.asteria.ouranos.core::Ouranos');
+        super(Ouranos.CLASS_NAME);
     }
     static createSession(config) {
+        this.checkConfig(config);
         return new OuranosSession_1.OuranosSession(config);
     }
     static buildProcess(processClass, config) {
         return OuranosProcessBuilder_1.OuranosProcessBuilder.getInstance().build(processClass, config);
     }
+    static checkConfig(config) {
+        let error = null;
+        if (config === null || config === undefined) {
+            error = OuranosErrorBuilder_1.OuranosErrorBuilder.getInstance().build(asteria_gaia_1.AsteriaErrorCode.MISSING_SESSION_CONFIG, Ouranos.CLASS_NAME, 'no configuration is specified');
+            OuranosLogger_1.OuranosLogger.getLogger().fatal(error.toString());
+        }
+        else {
+            const name = config.name;
+            if (config === null || config === undefined) {
+                error = OuranosErrorBuilder_1.OuranosErrorBuilder.getInstance().build(asteria_gaia_1.AsteriaErrorCode.MISSING_SESSION_NAME, Ouranos.CLASS_NAME, 'no session session name is specified');
+                OuranosLogger_1.OuranosLogger.getLogger().fatal(error.toString());
+            }
+            else if (typeof name !== asteria_gaia_1.PrimitiveType.STRING || name === asteria_gaia_1.CommonChar.EMPTY) {
+                error = OuranosErrorBuilder_1.OuranosErrorBuilder.getInstance().build(asteria_gaia_1.AsteriaErrorCode.INVALID_SESSION_NAME, Ouranos.CLASS_NAME, 'session config must define a valid session name');
+                OuranosLogger_1.OuranosLogger.getLogger().fatal(error.toString());
+            }
+        }
+        if (error) {
+            throw asteria_gaia_1.ErrorUtil.errorToException(error);
+        }
+    }
 }
+Ouranos.CLASS_NAME = 'com.asteria.ouranos.core::Ouranos';
 exports.Ouranos = Ouranos;
--- src/com/asteria/ouranos/core/Ouranos.ts
+++ src/com/asteria/ouranos/core/Ouranos.ts
@@ -1,6 +1,8 @@
-import { AbstractAsteriaObject, AsteriaSession, AsteriaSessionConfig, StreamProcess, StreamProcessConfig } from 'asteria-gaia';
+import { AbstractAsteriaObject, AsteriaSession, AsteriaSessionConfig, StreamProcess, StreamProcessConfig, AsteriaError, AsteriaErrorCode, PrimitiveType, CommonChar, ErrorUtil } from 'asteria-gaia';
 import { OuranosSession } from './OuranosSession';
 import { OuranosProcessBuilder } from '../util/builder/OuranosProcessBuilder';
+import { OuranosLogger } from '../util/logging/OuranosLogger';
+import { OuranosErrorBuilder } from '../util/builder/OuranosErrorBuilder';
 
 /**
  * The <code>Ouranos</code> class is the entry point of the Ouranos framework.
@@ -8,10 +10,15 @@ import { OuranosProcessBuilder } from '../util/builder/OuranosProcessBuilder';
 export class Ouranos extends AbstractAsteriaObject {
 
     /**
+     * The fully qualified name of this class.
+     */
+    private static readonly CLASS_NAME: string = 'com.asteria.ouranos.core::Ouranos';
+
+    /**
      * Create a new <code>Ouranos</code> instance.
      */
     constructor() {
-        super('com.asteria.ouranos.core::Ouranos');
+        super(Ouranos.CLASS_NAME);
     }
 
     /**
@@ -22,6 +29,7 @@ export class Ouranos extends AbstractAsteriaObject {
      * @returns {AsteriaSession} a new <code>AsteriaSession</code> object.
      */
     public static createSession(config: AsteriaSessionConfig): AsteriaSession {
+        this.checkConfig(config);
         return new OuranosSession(config);
     }
 
@@ -35,5 +43,42 @@ export class Ouranos extends AbstractAsteriaObject {
      */
     public static buildProcess(processClass: any, config?: StreamProcessConfig): StreamProcess {
         return OuranosProcessBuilder.getInstance().build(processClass, config);
+    }
+
+    /**
+     * Validate the specified config object.
+     * 
+     * @param {AsteriaSessionConfig} config the config object to validate.
+     */
+    private static checkConfig(config: AsteriaSessionConfig): void {
+        let error: AsteriaError = null;
+        if (config === null || config === undefined) {
+            error = OuranosErrorBuilder.getInstance().build(
+                AsteriaErrorCode.MISSING_SESSION_CONFIG,
+                Ouranos.CLASS_NAME,
+                'no configuration is specified'
+            );
+            OuranosLogger.getLogger().fatal(error.toString());
+        } else {
+            const name: any = config.name;
+            if (config === null || config === undefined) {
+                error = OuranosErrorBuilder.getInstance().build(
+                    AsteriaErrorCode.MISSING_SESSION_NAME,
+                    Ouranos.CLASS_NAME,
+                    'no session session name is specified'
+                );
+                OuranosLogger.getLogger().fatal(error.toString());
+            } else if (typeof name !== PrimitiveType.STRING || name === CommonChar.EMPTY) {
+                    error = OuranosErrorBuilder.getInstance().build(
+                        AsteriaErrorCode.INVALID_SESSION_NAME,
+                        Ouranos.CLASS_NAME,
+                        'session config must define a valid session name'
+                    );
+                    OuranosLogger.getLogger().fatal(error.toString()); 
+            }
+        }
+        if (error) {
+            throw ErrorUtil.errorToException(error);
+        }
     }
 }
